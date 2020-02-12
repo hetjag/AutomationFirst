667b938542a08733ace0e8141ba0d3dc4b138e1b
hetjag <Hetaldhameliya13@gmail.com>
2020-02-12 19:17:42 +0000

    WebFirstHomework1

--- src/main/java/WebFirst/WebFistHomework1.java
+++ src/main/java/WebFirst/WebFistHomework1.java
@@ -1,222 +1,248 @@
 package WebFirst;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-public class WebFistHomework1
-{
+public class WebFistHomework1 {
 
     static protected WebDriver driver;
+    //Wait for clickable method
+    public void waitForClickable(By by, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
 
-    public  void waitforClickable(By by, int time)
-        {
-            WebDriverWait wait = new WebDriverWait(driver,time);
-            wait.until(ExpectedConditions.elementToBeClickable(by));
-        }
+    //Wait for visibility method
+    public void waitForVisibility(By by, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
 
-    public  void waitforDisaplay(By by, int time)
-    {
-        WebDriverWait wait = new WebDriverWait(driver,time);
+    //wait for element present
+    public void waitForElementsPresent(By by, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+    //Wait for disaply method
+    public void waitforDisaplay(By by, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
         wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
 
-    public  void waitforDisaapear(By by, int time)
-    {
-        WebDriverWait wait = new WebDriverWait(driver,time);
+    //Wait for disaapear method
+    public void waitforDisaapear(By by, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
         wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
+
+    //generate time stamp in string
+    public String timeStamp() {
+        DateFormat dateFormat = new SimpleDateFormat("ddyyhhmmss");
+        Date date = new Date();
+        return dateFormat.format(date);
+
+    }
+
+
+    //get text from element
+    public String getTextFromElement(By by)
+    {
+        return driver.findElement(by).getText();
+    }
+
+    //click on element
+    public void clickOnElement(By by)
+    {
+        driver.findElement(by).click();
+    }
+
+    public void EnterText(By by, String text) {
+        driver.findElement(by).sendKeys(text);
+
+    }
+
+    // select by visible text
+    public void selectByVisibleText(By by, String text) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByVisibleText(text);
+    }
+
+    //
+    public void selectByIndex(By by, int number) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByIndex(number);
+    }
+
+    public void selectByValue(By by, String text) {
+        Select select = new Select(driver.findElement(by));
+        select.selectByValue(text);
+    }
+
+
+
+    // opening the browser
+    @Before
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\java\\BrowserDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+    }
+
+    //closing browser
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
 
     @Test
-    public void userShouldBeLoginSccessfully()
-    {
-        // opening the browser
-        System.setProperty("webdriver.chrome.driver","src\\test\\java\\BrowserDriver\\chromedriver.exe");
-        driver=new ChromeDriver();
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+    public void userShouldBeLoginSccessfully() {
+       String expect ="**Successful Login**";
         // opening url
         driver.get("http://thedemosite.co.uk/");
 
         // clicking on add user
-        driver.findElement(By.linkText("3. Add a User")).click();
+        clickOnElement(By.linkText("3. Add a User"));
 
         //type username
-        driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("rajpatel1");
+
+        String name = getTextFromElement(By.xpath("//input[@name=\"username\"]"));
+        EnterText(By.xpath("//input[@name=\"username\"]"), "rajpatel");
+
 
         //type password
-        driver.findElement(By.name("password")).sendKeys("Test1234");
+        EnterText(By.name("password"), "Test1234");
+
 
         //click on save
-        driver.findElement(By.name("FormsButton2")).click();
+        clickOnElement(By.name("FormsButton2"));
+
+      //  driver.findElement(By.id("ggfdhgfd")).getText();
 
         //click on login
-        driver.findElement(By.linkText("4. Login")).click();
+        clickOnElement(By.linkText("4. Login"));
+
 
         //type username
-        driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("rajpatel1");
+        EnterText(By.xpath("//input[@name=\"username\"]"), "rajpatel");
+
 
         //type password
-        driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("Test1234");
+        EnterText(By.xpath("//input[@name=\"password\"]"), "Test1234");
 
         //click on login
-        driver.findElement(By.name("FormsButton2")).click();
+        clickOnElement(By.name("FormsButton2"));
 
-        //closing browser
-        driver.quit();
+        String actual = getTextFromElement(By.xpath("//center/b"));
+       Assert.assertEquals("Successful Login",expect,actual);
+
 
     }
 
 
-
-
-
     @Test
-    public void userShouldBeLoginPanelSuccessfully()
-    {
-        // opening the browser
-        System.setProperty("webdriver.chrome.driver","src\\test\\java\\BrowserDriver\\chromedriver.exe");
-        driver=new ChromeDriver();
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void userShouldBeLoginPanelSuccessfully() {
+        String expected = "Welcome Admin";
 
         // opening url
         driver.get("https://opensource-demo.orangehrmlive.com/");
 
         //type username
-        driver.findElement(By.xpath("//input[@ name=\"txtUsername\"]")).sendKeys("admin");
-
+        EnterText(By.xpath("//input[@ name=\"txtUsername\"]"),"admin");
 
         //type password
-        driver.findElement(By.xpath("//input[@name=\"txtPassword\"]")).sendKeys("admin123");
+        EnterText(By.xpath("//input[@name=\"txtPassword\"]"),"admin123");
 
         //click on login
-        driver.findElement(By.id("btnLogin")).click();
+        clickOnElement(By.id("btnLogin"));
         System.out.println("login clicked");
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        //closing browser
-        driver.quit();
+        String actual = getTextFromElement(By.xpath("//a[@class='panelTrigger']"));
 
 
+        Assert.assertEquals("User is not able to login",expected,actual);
 
     }
 
-    @Test
-    public void userShouldCreateAnAccountSuccessfully()
-    {
-        // opening the browser
-        System.setProperty("webdriver.chrome.driver","src\\test\\java\\BrowserDriver\\chromedriver.exe");
-        driver=new ChromeDriver();
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        // opening url
-        driver.get("http://automationpractice.com/");
-
-        //click on login
-        driver.findElement(By.xpath("//a[@class=\"login\"]")).click();
-
-        //type email address
-        driver.findElement(By.xpath("//input[@id=\"email_create\" and @name=\"email_create\"]")).sendKeys("set@test");
-       // driver.findElement(By.xpath("//input[@name=\"email_create\"]")).sendKeys("john@test.com");
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////
-        //closing browser
-        driver.quit();
-    }
 
     @Test
-    public void userShouldBeSignInSuccessfully()
-    {
-        // opening the browser
-        System.setProperty("webdriver.chrome.driver","src\\test\\java\\BrowserDriver\\chromedriver.exe");
-        driver=new ChromeDriver();
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void userShouldBeSignInSuccessfully() {
 
+        String expectectErrorMessage = "Wrong email or password, please try again.";
         // opening url
         driver.get("https://www2.hm.com/en_gb/index.html");
 
         //clicking on ladies
-        driver.findElement(By.linkText("Ladies")).click();
+        clickOnElement(By.linkText("Ladies"));
 
         //clicking on cloth
-        driver.findElement(By.xpath("//li[1]/ul/li[1]/a")).click();
+        clickOnElement(By.xpath("//li[1]/ul/li[1]/a"));
+
 
         //click on sign
-        driver.findElement(By.xpath("//a[@data-remodal-trigger=\"signin\" and @class=\"menu__myhm\"]")).click();
+        clickOnElement(By.xpath("//a[@data-remodal-trigger=\"signin\" and @class=\"menu__myhm\"]"));
+
 
         //type email address
-        driver.findElement(By.name("j_username")).sendKeys("fhgfydsfd@mail.com");
+        EnterText(By.name("j_username"),"set+"+timeStamp()+"@netgmail.com");
 
         //type password
-        driver.findElement(By.name("j_password")).sendKeys("12345");
+        EnterText(By.name("j_password"),"12345");
+
 
         //click on sign in
-        driver.findElement(By.xpath("//button[contains(@class,\"btn-login\")]")).click();
+        clickOnElement(By.xpath("//button[contains(@class,\"btn-login\")]"));
 
-        //closing browser
-        driver.quit();
+        String actualErrorMessage = getTextFromElement(By.xpath("//span[@id=\"modal-txt-signin-password-unknown-error-type-error\"]"));
 
+        //verifying error message display
+        Assert.assertEquals("Un-registered user is able to login",expectectErrorMessage,actualErrorMessage);
 
     }
+
+
 
     @Test
     public void userShouldBeRegisterSuccessfully()
     {
-        // opening the browser
-        System.setProperty("webdriver.chrome.driver","src\\test\\java\\BrowserDriver\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false);
-
-        driver=new ChromeDriver(options);
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+        String expectedMsg ="Registering is quick and easy";
         // opening url
-        driver.get("http://way2automation.com/way2auto_jquery/index.php");
-
-        //type name
-        driver.findElement(By.xpath("//input[@name=\"name\"]")).sendKeys("ramesh");
-
-        //type phone
-        driver.findElement(By.name("phone")).sendKeys("09898989898");
+        driver.get("https://www.asda.com/register");
 
         //type email
-        driver.findElement(By.name("email")).sendKeys("s43535345dfdser@test.com");
-
-        //type city
-        driver.findElement(By.name("city")).sendKeys("Gujarat");
-
-      //  WebDriverWait wait = new WebDriverWait(driver,5);
-        //*[@id="load_form"]/fieldset[6]/input
-
-        //type  username
-        driver.findElement(By.xpath("//fieldset[6]/input")).sendKeys("fdsfds");
+        EnterText(By.xpath("//input[@type='email']"),"abcdefg12"+timeStamp()+"@gmail.com");
 
         //type password
-        driver.findElement(By.xpath("//fieldset[7]/input")).sendKeys("098765dfgffdsfdsdgfdfg4");
+        EnterText(By.xpath("//input[@type=\"password\"]"),"ab123456=");
 
-        //click on submit
-        driver.findElement(By.xpath("//form[@class=\"ajaxsubmit\"]/div/div[2]/input")).click();
+        //enter postcode
+        EnterText(By.xpath("//input[@type=\"text\"]"),"ha0 2hn");
 
-        // closing browser
-        driver.quit();
+          //click on box
+        clickOnElement(By.xpath("//label[1]/span[@class='checkmark']"));
+
+        //click on register
+        clickOnElement(By.xpath("//button[@type='submit']"));
+        String registerSuccessMesageActiual = getTextFromElement(By.xpath("//div/h1"));
+
+        //verifying register success message dislapying
+        Assert.assertTrue("Registration to asda is failed",registerSuccessMesageActiual.contains(expectedMsg));
 
     }
-
 }
